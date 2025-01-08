3811faede6ec21ec0eaa340c7fdb2067708ce0fa
Kylian Dupouy Peron <kdperon@yahoo.com>
2025-01-08 11:55:57 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { TextAnimate } from "@/components/ui/text-animate";
+
 export default function Home() {
   return (
     <main>
-      <h1>Accueil</h1>
+      <section className="flex flex-col gap-5 h-[calc(100vh-4rem)] justify-center items-center">
+        <span className="font-heading font-extrabold text-4xl">Amara Games</span>
+        <TextAnimate className="font-thin" animation="blurInUp" by="character">
+          Amara, c&apos;est bien en vrai
+        </TextAnimate>
+      </section>
+      <section className="h-screen"></section>
     </main>
   );
 }
