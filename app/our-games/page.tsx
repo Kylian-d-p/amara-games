16902d3fb4c3fb67508569c0fe7cacdd56b762fc
maeld <mael.delattre@efrei.net>
2025-01-10 15:57:05 +0100

    update

--- app/our-games/page.tsx
+++ app/our-games/page.tsx
@@ -1,7 +1,16 @@
 export default function OurGames() {
     return (
-        <main>
+        <main className="flex flex-col items-center pt-5">
             <h1>Nos Jeux</h1>
+            <div className="flex flex-row">
+                <div className="">
+                </div>
+                
+                <div className= " bg-white w-1 h-screen" >
+                </div>
+
+                <div></div>
+            </div>
         </main>
     )
 }
