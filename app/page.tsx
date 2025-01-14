bf2e166cd2286d81243bcfbabd0d5bd40ac29fc6
Kylian Dupouy Peron <kdperon@yahoo.com>
2025-01-14 08:33:53 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { TextAnimate } from "@/components/ui/text-animate";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -13,9 +14,9 @@ export default function Home() {
         </video>
       </section>
       <section className="bg-secondary py-16 flex justify-center">
-        <article className="px-10 flex flex-col gap-8 max-w-4xl">
+        <article className="px-5 lg:px-10 flex flex-col gap-8 max-w-3xl lg:max-w-6xl">
           <h2 className="text-2xl font-bold">Un projet ambitieux</h2>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col lg:flex-row items-center gap-5">
             <div className="flex flex-col gap-3">
               <p>
                 Chez <span className="font-heading font-extrabold">Amara Games</span>, nous rêvons grand et jouons encore plus grand.
@@ -34,28 +35,32 @@ export default function Home() {
                 ambition : redonner au jeu vidéo sa place de rêveur et de raconteur d&apos;histoires. Ensemble, bâtissons le futur du gaming.
               </p>
             </div>
+            <Image src="/images/gaming-controller.jpg" width={1280} height={851} alt="Gaming controller" className="rounded-lg md:max-w-md" />
           </div>
         </article>
       </section>
-      <section className="bg-background py-16 flex justify-center">
-        <article className="px-10 flex flex-col gap-8 max-w-4xl">
+      <section className="py-16 flex justify-center">
+        <article className="px-5 lg:px-10 flex flex-col gap-8 max-w-3xl lg:max-w-6xl">
           <h2 className="text-2xl font-bold">Des talents visionnaires</h2>
-          <div className="flex flex-col gap-3">
-            <p>
-              Chez <span className="font-heading font-extrabold">Amara Games</span>, nous croyons que derrière chaque grand jeu se cache une équipe de
-              talents exceptionnels.
-            </p>
-            <p>
-              Notre priorité ? Identifier, soutenir et mettre en lumière des créateurs visionnaires capables de repousser les limites du possible.
-            </p>
-            <p>
-              Qu’il s’agisse de studios indépendants ou de jeunes développeurs au potentiel éclatant, nous leur offrons les outils, les ressources et
-              la liberté créative nécessaires pour donner vie à leurs idées les plus audacieuses.
-            </p>
-            <p>
-              Ici, chaque voix compte, chaque innovation est célébrée, et chaque projet est l’occasion d’écrire une nouvelle page de l’histoire du jeu
-              vidéo. Vous avez une vision ? Nous sommes là pour la propulser.
-            </p>
+          <div className="flex flex-col-reverse lg:flex-row items-center gap-5">
+            <Image src="/images/esport.webp" width={1800} height={1200} alt="Gaming controller" className="rounded-lg md:max-w-md" />
+            <div className="flex flex-col gap-3">
+              <p>
+                Chez <span className="font-heading font-extrabold">Amara Games</span>, nous croyons que derrière chaque grand jeu se cache une équipe
+                de talents exceptionnels.
+              </p>
+              <p>
+                Notre priorité ? Identifier, soutenir et mettre en lumière des créateurs visionnaires capables de repousser les limites du possible.
+              </p>
+              <p>
+                Qu’il s’agisse de studios indépendants ou de jeunes développeurs au potentiel éclatant, nous leur offrons les outils, les ressources
+                et la liberté créative nécessaires pour donner vie à leurs idées les plus audacieuses.
+              </p>
+              <p>
+                Ici, chaque voix compte, chaque innovation est célébrée, et chaque projet est l’occasion d’écrire une nouvelle page de l’histoire du
+                jeu vidéo. Vous avez une vision ? Nous sommes là pour la propulser.
+              </p>
+            </div>
           </div>
         </article>
       </section>
